82816aff7f3179a926480820aecc2da061fa4d83
prabaharanbalaji16 <code.development16@gmail.com>
2022-11-13 14:30:53 +0530

    Part-1 Week-2

---
+++ Learning/Part-1/Week-2/Assignments/Permutation.java
@@ -0,0 +1,23 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int count = Integer.parseInt("3");
+        RandomizedQueue rq = new RandomizedQueue<>();
+
+        Scanner s = new Scanner(System.in);
+
+        for(int i=0;i<10;i++)
+        {
+            String item = s.nextLine();
+            rq.enqueue(item);
+        }
+        s.close();
+        for(int i=0;i<count;i++)
+        {
+            System.out.println(rq.dequeue());
+        }
+    }
+}
